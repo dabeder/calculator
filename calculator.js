55b9d2fbf237c9e7b8021dd78f5cc97084e6eca9
Daniel Beder <daniel.beder@gmail.com>
2021-03-01 16:43:26 -0600

    Make decimal point functional

--- calculator.js
+++ calculator.js
@@ -28,6 +28,12 @@ const clearFunctionKeys = () => {
     })
 }
 
+const findDecimal = (string) => {
+    let dotLocation = string.search(/[.]/g);
+    if (dotLocation == -1) return false;
+    else return dotLocation;
+}
+
 clear();
 updateDisplay("0");
 
@@ -47,8 +53,8 @@ const divide = (number1, number2) => {
 
 const operate = (number1, operator, number2) => {
     console.log(`Calculate: ${number1}, ${number2}, ${operator}`);
-    number1 = parseInt(number1);
-    number2 = parseInt(number2);
+    number1 = parseFloat(number1);
+    number2 = parseFloat(number2);
     let result;
     switch (operator) {
         case "add":
@@ -71,10 +77,22 @@ const operate = (number1, operator, number2) => {
         updateDisplay("ERROR");
     }
     else {
-        result = Math.round(parseFloat(result));
-        if (result.toString().length > 15) {
-            updateDisplay("OVERFLOW");
-            clear();
+        resultString = result.toString();
+        if (resultString.length > 15) {
+            if ((findDecimal(resultString)>16) || (!findDecimal(resultString))) { // more than 15 digits left of the decimal, so display as overflow
+                updateDisplay("OVERFLOW");
+                clear();
+            }
+            else { // figure out how many to round down
+                let leftDigits = findDecimal(resultString);
+                let digitsToRound = 14-leftDigits;
+                let temp = Math.round(result * Math.pow(10, digitsToRound)) / Math.pow(10, digitsToRound);
+                result = temp;
+                updateDisplay(result);
+                input1 = result;
+                input2 = '';
+                operator = '';
+            }
         }
         else {
             updateDisplay(result);
@@ -109,14 +127,18 @@ digitButtons.forEach(button => {
         }
         if (temp.length >= 15) {
             console.log('out of space');
+            // change css to flash display box or something
             return;
         }
         else { // still fits on screen
             if (button.getAttribute('data-button')==="decimal") {
+                if (findDecimal(temp)) return; // if a decimal point already exists, can't add another
                 console.log('point'); //TODO. also standardize hover/active css
-                return;
+                if (temp == '') {
+                    temp = '0';
+                }
             }
-            input2 = parseInt(temp + button.innerText);
+            input2 = temp + button.innerText;
             updateDisplay(input2);
             prevButtonType = "numberKey";
             console.log(`Inputs are ${input1} and ${input2}, operator is ${operator}`);
